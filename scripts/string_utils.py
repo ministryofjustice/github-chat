5c574b9266bc24bced5f073ed9b1ff587dbcc8a4
Richard Leyshon <richard.leyshon@justice.gov.uk>
2024-12-13 14:51:01 +0000

    refactor: Reduce lines

--- scripts/string_utils.py
+++ scripts/string_utils.py
@@ -10,25 +10,16 @@ def format_results(
     model_summary:str,
     template:str=RESPONSE_TEMPLATE
     ) -> str:
-    org = db_result.get("org_nm")
-    nm = db_result.get("repo_nm")
-    desc = db_result.get("repo_desc")
-    url = db_result.get("html_url")
-    priv = db_result.get("is_private")
-    arch = db_result.get("is_archived")
-    upd_at = db_result.get("updated_at")
-    prog_lang = db_result.get("programming_language")
-    dist = db_result.get("distance")
     return template.format(
-        org_nm = org,
-        repo_nm=nm,
-        repo_desc=desc,
-        url=url,
-        is_private=priv,
-        is_archived=arch,
-        updated_at=upd_at,
-        programming_language=prog_lang,
-        distance=dist,
+        org_nm = db_result.get("org_nm"),
+        repo_nm=db_result.get("repo_nm"),
+        repo_desc=db_result.get("repo_desc"),
+        url=db_result.get("html_url"),
+        is_private=db_result.get("is_private"),
+        is_archived=db_result.get("is_archived"),
+        updated_at=db_result.get("updated_at"),
+        programming_language=db_result.get("programming_language"),
+        distance=db_result.get("distance"),
         model_summary=model_summary
         )
 
