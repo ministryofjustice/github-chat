a6258472eb785e03a99fd3ad13db3cb91cfce75b
Richard Leyshon <richard.leyshon@justice.gov.uk>
2025-01-15 11:59:43 +0000

    refactor: Model config values clearly seperated between app & pipeline

--- scripts/pipeline_config.py
+++ scripts/pipeline_config.py
@@ -3,6 +3,5 @@ from pyprojroot import here
 COLLECTION_NM = "moj-github"
 EMBEDDINGS_MODEL = "nomic-embed-text"
 REPO_LLM = "gpt-4o-mini" # for ai summaries of repos
-META_LLM = "gpt-4o-2024-11-20"
 VECTOR_STORE_PTH = here("data/nomic-embeddings")
 TEMP = 1.0
