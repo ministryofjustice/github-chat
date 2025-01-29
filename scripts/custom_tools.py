3369c729a0296f2d053b2ee813d93c8647adf344
Richard Leyshon <richard.leyshon@justice.gov.uk>
2025-01-29 10:42:26 +0000

    feat: Register new tool for data export

--- scripts/custom_tools.py
+++ scripts/custom_tools.py
@@ -5,6 +5,35 @@ from openai import pydantic_function_tool
 from pydantic import BaseModel
 
 class ExtractKeywordEntities(BaseModel):
+    """
+    A model for extracting keyword entities.
+
+    Pass a list of keyword strings extracted from the user's prompt.
+
+    Attributes
+    ----------
+    keywords : List[str]
+        A list of keywords to be extracted.
+    """
+
     keywords: List[str]
 
-toolbox = [pydantic_function_tool(ExtractKeywordEntities)]
+
+class ExportDataToTSV(BaseModel):
+    """Export cached data to a TSV file.
+
+    Choose a filename that makes sense based upon the user's queries.
+
+    Attributes
+    ----------
+    filename : str
+        The name of the TSV file to which data will be exported. Must end
+        in a ".tsv" suffix.
+    """
+
+    filename: str
+
+toolbox = [
+    pydantic_function_tool(ExtractKeywordEntities),
+    pydantic_function_tool(ExportDataToTSV)
+    ]
